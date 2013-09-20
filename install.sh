7cc4b9acc913f89398009332f3cc5231a3b20719
John Karlsson <tartrate@gmail.com>
2013-09-20 19:51:55 +0200

    Minimal install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Moves old dotfiles to ~/.dotfiles_old and replaces them with symlinks to here.
+
+dir=~/dotfiles
+olddir=~/.dotfiles_old
+files="bashrc vimrc vim zshrc oh-my-zsh"
+
+mkdir -p $olddir
+
+cd $dir
+
+for file in $files; do
+    mv ~/.$file ~/dotfiles_old/
+    ln -s $dir/.$file ~/.$file
+done
