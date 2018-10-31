9c3314340055e6b7f2febe28701e190e18578f26
John Karlsson <tartrate@gmail.com>
2018-10-31 13:09:52 +0100

    BT

--- .tmux/bt.sh
+++ .tmux/bt.sh
@@ -1,5 +1,12 @@
 # ᛡᛒ
 #!/bin/bash
 COL=$(if lsusb | grep -q "Intel Corp."; then echo "#00ff00"; else echo "colour240"; fi)
-printf "[#[fg=$COL]"
-printf "B#[default]]"
+DEV=$(script -c bluetoothctl <<< quit | grep -Pom1 '(?<=\[)[^\]\[]+(?=])' | sed -r 's/bluetooth//')
+printf "["
+printf "#[fg=$COL]BT#[default]"
+if [ ! -z "$DEV" ]; then
+	printf " ("
+	printf "#[fg=blue]$DEV#[default]"
+	printf ")"
+fi
+printf "]"
