518cf7cc595cf3f7f767a396985a4ad6fad2c045
John Karlsson <john.karlsson@databricks.com>
2024-10-01 07:41:23 +0200

    ..

--- .config/nvim/lua/john/plugins/substitute.lua
+++ .config/nvim/lua/john/plugins/substitute.lua
@@ -8,9 +8,9 @@ return {
 
         local keymap = vim.keymap
 
-        vim.keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-        vim.keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-        vim.keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-        vim.keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+        vim.keymap.set("n", "gs", substitute.operator, { desc = "Substitute with motion" })
+        vim.keymap.set("n", "gss", substitute.line, { desc = "Substitute line" })
+        vim.keymap.set("n", "gS", substitute.eol, { desc = "Substitute to end of line" })
+        vim.keymap.set("x", "gs", substitute.visual, { desc = "Substitute in visual mode" })
     end,
 }
