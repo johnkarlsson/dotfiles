f81f5dff28eb208278ee23d39ffbc8fdc75e81d1
John Karlsson <tartrate@gmail.com>
2024-10-01 23:09:24 +0200

    ...

--- .config/nvim/lua/john/core/keymaps.lua
+++ .config/nvim/lua/john/core/keymaps.lua
@@ -4,6 +4,15 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader> ", ":nohl<CR>", { desc = "Clear search highlights" })
 
+keymap.set('n', '<M-h>', '<C-w>h', { noremap = true, silent = true })
+keymap.set('n', '<M-j>', '<C-w>j', { noremap = true, silent = true })
+keymap.set('n', '<M-k>', '<C-w>k', { noremap = true, silent = true })
+keymap.set('n', '<M-l>', '<C-w>l', { noremap = true, silent = true })
+keymap.set('n', '<M-H>', '<C-w>H', { noremap = true, silent = true })
+keymap.set('n', '<M-J>', '<C-w>J', { noremap = true, silent = true })
+keymap.set('n', '<M-K>', '<C-w>K', { noremap = true, silent = true })
+keymap.set('n', '<M-L>', '<C-w>L', { noremap = true, silent = true })
+
 -- keymap.set("n", "<leader>s", ":lua require('nvim-treesitter.incremental_selection').node_incremental()<CR>", { desc = "Treesitter Incremental Selection" })
 -- keymap.set({"n", "v", "i"}, "<leader>s", ":lua require('nvim-treesitter.incremental_selection').node_incremental()<CR>", { desc = "Treesitter Incremental Selection" })
 -- keymap.set("v", "<leader>s", ":lua require('nvim-treesitter.incremental_selection').init_selection()<CR>", { desc = "Treesitter Incremental Selection" })
