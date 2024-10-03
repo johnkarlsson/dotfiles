17b7cda3ce0df1e288d1d0f67be200ba19c43a1c
John Karlsson <john.karlsson@databricks.com>
2024-10-03 13:52:48 +0200

    ..

--- .config/nvim/lua/john/plugins/colorscheme.lua
+++ .config/nvim/lua/john/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
         -- local bg = "#011628"
         local bg_dark = "#011423"
         -- local bg_highlight = "#143652"
-        -- local bg_search = "#0A64AC"
+        local bg_search = "#CC5C5C"
         -- local bg_visual = "#275378"
         local fg = "#CBE0F0"
         -- local fg_dark = "#B4D0E9"
@@ -21,7 +21,7 @@ return {
                 colors.bg_float = bg_dark
                 -- colors.bg_highlight = bg_highlight
                 -- colors.bg_popup = bg_dark
-                -- colors.bg_search = bg_search
+                colors.bg_search = bg_search
                 -- colors.bg_sidebar = bg_dark
                 -- colors.bg_visual = bg_visual
                 -- colors.border = border
@@ -30,7 +30,7 @@ return {
                 colors.fg_gutter = fg_gutter
                 -- colors.fg_sidebar = fg_dark
                 colors.bg_statusline = "none"
-                colors.bg_search = "none"
+                -- colors.bg_search = "none"
                 colors.bg_sidebar = "none"
                 colors.bg = "none"
              end
