0fbd03032d66e7ae3ff0ad366aaa4c5b1864e894
John Karlsson <tartrate@gmail.com>
2026-01-20 20:29:50 +0100

    ...

--- .config/nvim/lua/john/core/gitrelated.lua
+++ .config/nvim/lua/john/core/gitrelated.lua
@@ -10,7 +10,7 @@ local function autosave_if_git()
 end
 
 -- Autosave when leaving insert mode - if in a git repo
-vim.api.nvim_create_autocmd("TextChanged", {
+vim.api.nvim_create_autocmd({"InsertLeave", "TextChanged"}, {
     pattern = "*",
     callback = autosave_if_git,
 })
