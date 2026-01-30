139168835beab6af45344dd942c00ef4589bd5c4
John Karlsson <john.karlsson@databricks.com>
2026-01-30 16:23:10 +0100

    ...

--- .config/nvim/lua/john/core/options.lua
+++ .config/nvim/lua/john/core/options.lua
@@ -63,6 +63,15 @@ end
 
 opt.wrap = false
 
+-- Enable text wrapping for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true  -- wrap at word boundaries
+  end,
+})
+
 opt.ignorecase = true
 opt.smartcase = true
 
