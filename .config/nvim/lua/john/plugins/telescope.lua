29dbdf36d6876a72bc4468bef9fc4e830efd1938
John Karlsson <tartrate@gmail.com>
2026-02-21 12:07:55 +0100

    ...

--- .config/nvim/lua/john/plugins/telescope.lua
+++ .config/nvim/lua/john/plugins/telescope.lua
@@ -79,6 +79,7 @@ return {
         keymap.set("n", "<leader>ft", function() builtin.live_grep { default_text = "- \\[ \\]" } end, { desc = "Search unchecked todos" })
         keymap.set("n", "<leader>fq", "<cmd>Telescope quickfix<cr>", { desc = "Fuzzy find files in quickfix list" })
         keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+        keymap.set("n", "<leader>fF", function() builtin.find_files({ no_ignore = true, file_ignore_patterns = { "%.git/", "%.venv/", "node_modules/" } }) end, { desc = "Find files (incl. gitignored)" })
         keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find open buffers" })
         keymap.set("n", "<leader>fh", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
         keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
